090740c742be6979f7241358e5ef43727e5086b7
Janne Kujanpää <janne.kujanpaa@productivityleap.com>
2026-05-23 16:33:33 +0300

    init

---
+++ Azoo.psm1
@@ -0,0 +1,10 @@
+Get-ChildItem -Recurse "$PSScriptRoot/public/*.ps1" |
+    ForEach-Object { . $_.FullName }
+
+Get-ChildItem -Recurse "$PSScriptRoot/private/*.ps1" |
+    ForEach-Object { . $_.FullName }
+
+Export-ModuleMember -Function (
+    Get-ChildItem -Recurse "$PSScriptRoot/public/*.ps1" |
+    ForEach-Object BaseName
+)
---
+++ public/pim/Enable-AzooAzPimRoles.ps1
@@ -0,0 +1,76 @@
+function Enable-AzooAzPimRoles {
+[CmdletBinding(SupportsShouldProcess)]
+param (
+    [Parameter(Mandatory = $True)]
+    [String]
+    $justification,
+    [Parameter()]
+    [String]
+    $principalId = (Get-AzADUser -SignedIn).Id,
+    [Parameter()]
+    [String]
+    $expirationDuration = "PT30M",
+    [Parameter()]
+    [Scriptblock]
+    $filter,
+    [Parameter()]
+    [Switch]
+    $disableInteraction,
+    [Parameter()]
+    [Switch]
+    $force,
+    [Parameter()]
+    [Switch]
+    $forceNonInteractive
+)
+
+  $eligibleAzureRbac = Get-AzRoleEligibilityScheduleInstance -Scope "/" -Filter "asTarget()"
+
+  if ($filter) {
+    $filteredAzureRbac = $eligibleAzureRbac | Where-Object $filter
+  } else {
+    $filteredAzureRbac = $eligibleAzureRbac
+  }
+
+  if (-not $disableInteraction -and $PSVersionTable.PSEdition -eq "Desktop") {
+    Write-Host "Displaying Out-GridView for manual selection"
+    $selectedAzureRbac = $filteredAzureRbac | Out-GridView -PassThru
+  } elseif (-not $disableInteraction) {
+    $selectedAzureRbac = $filteredAzureRbac | Select-Object MemberType, PrincipalType, ScopeDisplayName, PrincipalDisplayName, RoleDefinitionDisplayName,id  | Out-ConsoleGridview -Title "Roles to select"
+  } else {
+    $selectedAzureRbac = $filteredAzureRbac
+  }
+
+  Write-Debug "$($selectedAzureRbac.Count) rows selected"
+
+  $continueIfInteractionDisabled = if ($disableInteraction) {
+      $WhatIfPreference -or $forceNonInteractive -or $PSCmdlet.ShouldContinue("Interaction disabled", "x")
+    } else {
+      $false
+    }
+
+  $selectedAzureRbac | ForEach-Object {
+    $eligibleRole = $_
+    $originalEligibleAzureRbac = $eligibleAzureRbac | Where-Object {$_.Id -eq $eligibleRole.Id }
+    $params = @{
+      Name = New-Guid
+      
+      Scope = $originalEligibleAzureRbac.Scope
+      Justification = $justification
+      LinkedRoleEligibilityScheduleId =  $originalEligibleAzureRbac.RoleEligibilityScheduleId
+      PrincipalId = $principalId
+      RequestType = "SelfActivate"
+      ExpirationDuration = "PT8H"
+      ExpirationType = "AfterDuration"
+      RoleDefinitionId = $originalEligibleAzureRbac.RoleDefinitionId
+    }
+
+    if (($disableInteraction -and $continueIfInteractionDisabled -or -not $disableInteraction) -and `
+    $PSCmdlet.ShouldProcess(
+      "Enabling RBAC role for $($originalEligibleAzureRbac.RoleEligibilityScheduleId)", 
+      $null, $null
+      )) {
+      New-AzRoleAssignmentScheduleRequest @params -Confirm:$false
+    }
+  }
+}
